69f9827fcd4ea54c9d46814a1ce64d88838ab5ca
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-14 15:11:23 -0300

    criado get all e post de actor usando DRF e CBV

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from actors.models import Actor
+from actors.serializers import ActorSerializer
 
 # Create your views here.
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
