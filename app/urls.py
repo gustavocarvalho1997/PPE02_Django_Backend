6c2fd8a3aef43add510a087a714f3183714537b9
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-22 16:32:16 -0300

    refatorado o codigo para organizacao e versionamento do app movies

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 urlpatterns = [
@@ -24,9 +23,6 @@ urlpatterns = [
 
     path('api/v1/', include('genres.urls')),
     path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),    
 
-    
-
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
 ]
