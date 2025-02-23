08f82a22317919bb93ddf5dcfc863ff3ff84a34a
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-23 17:46:57 -0300

    incluido calculo de media de avaliacao no serializer de Movie

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,17 +1,24 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        return round(rate,1) if rate else None
+
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError('A data de lançamento deve ser maior que 1990.')
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento deve ser maior que 1900.')
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
-            raise serializers.ValidationError('O resumo deve ter no máximo 200 caracteres.')
+        if len(value) > 500:
+            raise serializers.ValidationError('O resumo deve ter no máximo 500 caracteres.')
         return value
