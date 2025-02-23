89ee5a9006cfb2f2afd61e7d9a95c957aafac55a
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-23 16:47:04 -0300

    validate no serializar de Movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,3 +5,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento deve ser maior que 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('O resumo deve ter no máximo 200 caracteres.')
+        return value
