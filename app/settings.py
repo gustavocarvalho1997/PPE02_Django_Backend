99b7b882f6d60707fe89d369917ec99a62f48b6e
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-24 17:30:06 -0300

    configure JWT authentication and add authentication URLs

--- app/settings.py
+++ app/settings.py
@@ -130,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
